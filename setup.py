a102703fab44bc6bb88f11c283bcbbcea59ab02b
Sachidananda Thimmappaya <sachin_mt@hotmail.com>
2025-12-31 17:03:01 -0500

    setup changed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dab_project",
-    version="0.0.1",
+    version="0.0.2",
     description="This contains the code in the ./src directory of the project",
     author="Sachidananda Thimmappaya",
     packages=find_packages(where="./src"),
